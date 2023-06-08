82522594eda8326fec54f5683a0c60f84098bd17
Yauri Attamimi <yauritux@gmail.com>
2023-06-09 06:16:48 +0700

    refactor for DRY, move navbar to root layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import NavBar from "@/app/components/NavBar";
 import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
@@ -10,24 +9,21 @@ import ReservationCard from "./components/ReservationCard";
 
 export default function RestaurantDetails() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar/>
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[70%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Title />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className="w-[27%] relative text-reg">
-            <ReservationCard />
-          </div>
+    <>
+      <Header />
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[70%] rounded p-3 shadow">
+          <RestaurantNavBar />
+          <Title />
+          <Rating />
+          <Description />
+          <Images />
+          <Reviews />
         </div>
-      </main>
-    </main>
+        <div className="w-[27%] relative text-reg">
+          <ReservationCard />
+        </div>
+      </div>
+    </>        
   )
 }
