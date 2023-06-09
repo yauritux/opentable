d05725d8183dee1c76a38b942b3156e2f20cd3e2
Yauri Attamimi <yauritux@gmail.com>
2023-06-09 16:50:39 +0700

    integrate sidebar filter with database

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,20 +1,31 @@
-export default function SearchSideBar() {
+import { prisma } from "@/pages/api/db"
+import { Location, Cuisine } from "@prisma/client"
+
+const fetchLocations = (): Promise<Location> => {
+  return prisma.location.findMany({})
+}
+
+const fetchCuisines = (): Promise<Cuisine> => {
+  return prisma.cuisine.findMany({})
+}
+
+export default async function SearchSideBar() {
+  const locations = await fetchLocations()
+  const cuisines = await fetchCuisines()
+
   return (
     <div className="w-1/5">
       <div className="border-b pb-4">
         <h1 className="mb-2">Region</h1>
-        <p className="font-light text-reg">Toronto</p>
-        <p className="font-light text-reg">Ottawa</p>
-        <p className="font-light text-reg">Montreal</p>
-        <p className="font-light text-reg">Hamilton</p>
-        <p className="font-light text-reg">Kingston</p>
-        <p className="font-light text-reg">Niagara</p>
+        {locations.map((location: Location) => (
+          <p key={location.id} className="font-light text-reg capitalize">{location.name}</p>
+        ))}        
       </div>
       <div className="border-b pb-4 mt-3">
         <h1 className="mb-2">Cuisine</h1>
-        <p className="font-light text-reg">Mexican</p>
-        <p className="font-light text-reg">Italian</p>
-        <p className="font-light text-reg">Chinese</p>
+        {cuisines.map((cuisine: Cuisine) => (
+          <p key={cuisine.id} className="font-light text-reg capitalize">{cuisine.name}</p>
+        ))}
       </div>
       <div className="mt3 pb-4">
         <h1 className="mb-2">Price</h1>
