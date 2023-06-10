bae630138935b116cb19f1157d8b5ade25debf09
Yauri Attamimi <yauritux@gmail.com>
2023-06-10 16:45:13 +0700

    add missing rating and number of reviews statistics

--- app/restaurant/[slug]/components/Rating.tsx
+++ app/restaurant/[slug]/components/Rating.tsx
@@ -1,12 +1,13 @@
-export default function Rating() {
+import { calculateReviewRating } from "@/utils/calculateReviewRating"
+import { Review } from "@prisma/client"
+
+export default function Rating({reviews} : {reviews: Review[]}) {
   return (
     <div className="flex items-end">
       <div className="ratings mt-2 flex items-center">
         <p className="text-[#dccc11] font-bold text-2xl">*****</p>
-        <p className="text-reg ml-3 text-[#000099] font-bold">4.9</p>
-      </div>
-      <div>
-        <p className="text-reg ml-4 text-[#000099] font-bold">600 Reviews</p>
+        <p className="text-reg ml-3 text-[#000099] font-bold">{calculateReviewRating(reviews).toFixed(1)}</p>
+        <p className="text-reg ml-4 text-[#000099] font-bold">{reviews.length} Review{reviews.length > 1 ? "s" : ""}</p>
       </div>
     </div>
   )
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -43,7 +43,7 @@ export default async function RestaurantDetails({params} : {params: { slug: stri
       <div className="bg-white w-[70%] rounded p-3 shadow">
         <RestaurantNavBar slug={restaurant.slug} />
         <Title name={restaurant.name} />
-        <Rating />
+        <Rating reviews={restaurant.reviews} />
         <Description description={restaurant.description}/>
         <Images images={restaurant.images} />
         <Reviews reviews={restaurant.reviews} />
